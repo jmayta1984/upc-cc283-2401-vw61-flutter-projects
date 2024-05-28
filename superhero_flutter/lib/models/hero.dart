f058a37a4bbaabd9cd6f48612c8dcbe9c1a041ca
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2024-05-27 21:54:08 -0500

    Add files via upload

--- superhero_flutter/lib/models/hero.dart
+++ superhero_flutter/lib/models/hero.dart
@@ -1,13 +1,26 @@
 class SuperHero {
+  final String id;
   final String name;
   final String fullName;
   final String path;
 
   const SuperHero(
-      {required this.name, required this.fullName, required this.path});
+      {required this.id,
+      required this.name,
+      required this.fullName,
+      required this.path});
 
   SuperHero.fromJson(Map<String, dynamic> json)
-      : name = json["name"],
+      : id = json["id"],
+        name = json["name"],
         fullName = json["biography"]["full-name"],
         path = json["image"]["url"];
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "name": name,
+      "full_name": fullName,
+    };
+  }
 }
